9b7ca09be3ac1f55c38b373fef1c33becf3f7189
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-08-30 10:29:48 +0545

    feat: custom hook used

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// import { useState } from 'react'
+import useCounter from './useCounter'
+const App = (props) => {
+  // const [counter, setCounter] = useState(0)
 
-function App() {
+  const counter = useCounter();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>{counter.value}</div>
+      <button onClick={counter.increase}>
+        plus
+      </button>
+      <button onClick={counter.decrease}>
+        minus
+      </button>      
+      <button onClick={counter.zero}>
+        zero
+      </button>
     </div>
-  );
+  )
 }
 
 export default App;
