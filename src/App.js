bc28481bc10496fd18dd9377351c7c6f269f6d13
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-08-30 10:33:56 +0545

    feat: button counter added

--- src/App.js
+++ src/App.js
@@ -4,19 +4,37 @@ const App = (props) => {
   // const [counter, setCounter] = useState(0)
 
   const counter = useCounter();
+  const buttonCounter = useCounter();
 
   return (
     <div>
-      <div>{counter.value}</div>
-      <button onClick={counter.increase}>
-        plus
-      </button>
-      <button onClick={counter.decrease}>
-        minus
-      </button>      
-      <button onClick={counter.zero}>
-        zero
-      </button>
+      <div>
+        <h1>Counter</h1>
+        <div>{counter.value}</div>
+        <button onClick={counter.increase}>
+          plus
+        </button>
+        <button onClick={counter.decrease}>
+          minus
+        </button>      
+        <button onClick={counter.zero}>
+          zero
+        </button>
+      </div>
+
+      <div>
+        <h1>Button counter</h1>
+        <div>{buttonCounter.value}</div>
+        <button onClick={buttonCounter.increase}>
+          plus
+        </button>
+        <button onClick={buttonCounter.decrease}>
+          minus
+        </button>      
+        <button onClick={buttonCounter.zero}>
+          zero
+        </button>
+      </div>
     </div>
   )
 }
